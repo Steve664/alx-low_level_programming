40dd31a8ea5e79c5a9fa8bd36321efe1ede4451b
Steve kariuki <stevekariuki5@gmail.com>
2022-07-01 18:56:26 +0000

    Added 2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Prints alphabet 
+*
+* Return: Always (Success)
+*/
+int main(void)
+{
+	char a;
+
+	for (a = 'a' ; a <= 'z' ; a++)
+	{
+		putchar(a);
+		putchar("\n");
+	}
+	return (0);
+}
